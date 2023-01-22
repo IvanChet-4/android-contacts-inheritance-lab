33846bc788523ed6a4866c41ed25be2749e23c0f
IvanChet-4 <github_isc@list.ru>
2023-01-22 15:33:35 +0300

    Update ContactsOrderTypeUi; Update SortTypeUI; Update FilterContactTypeUi;

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
@@ -2,9 +2,9 @@ package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
 
-public class ContactsOrderTypeUi {
-    String type;
-    boolean isSelected;
+public abstract class ContactsOrderTypeUi {
+    protected final String type;
+    protected final boolean isSelected;
 
     public ContactsOrderTypeUi(@NonNull String type, boolean isSelected) {
         this.type = type;
@@ -18,5 +18,7 @@ public class ContactsOrderTypeUi {
     public boolean isSelected() {
         return isSelected;
     }
+
+   public abstract String createLogMessage();
 
 }
